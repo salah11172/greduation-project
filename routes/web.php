359a0828f41640082a9ef7f58dc729ddcb9dcbb3
“ <salahmustafa11172@gmail.com>
2021-08-04 00:57:57 +0200

    try branch

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layout.layout');
 });
+Route::get('/try', function () {
+    return view('layout.layout');
+});
